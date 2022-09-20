cbcdb608532637bc4d7adee173e80cd5a7b42228
MrEnigmamgine "Joshua Mayes <mayes.joshua.d@gmail.com>
2022-09-20 15:32:37 -0400

    outlier handling

--- wrangle.py
+++ wrangle.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
-from sklearn.preprocessing import MinMaxScaler
+# from sklearn.preprocessing import MinMaxScaler
 
 import acquire
 from helpers.util import make_valid_py_id
